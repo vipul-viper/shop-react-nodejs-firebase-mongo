b6b46b5bb742dda82b5e92fc2d29d2587f1e393d
Vipul Saklani <vipul.cse17@nituk.ac.in>
2020-07-30 12:31:39 +0530

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,13 +1,13 @@
 import firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDGlsDsS1dQinwQg_LJfimgzUia4nQOP3M",
-    authDomain: "shop-beadd.firebaseapp.com",
-    databaseURL: "https://shop-beadd.firebaseio.com",
-    projectId: "shop-beadd",
-    storageBucket: "shop-beadd.appspot.com",
-    messagingSenderId: "683266808524",
-    appId: "1:683266808524:web:a4a83663bc6ce3c0914069"
+    apiKey: "4nQOP3M",
+    authDomain: "shom",
+    databaseURL: "https://shop-rebaom",
+    projectId: "shop",
+    storageBucket: "shop-t.com",
+    messagingSenderId: "683524",
+    appId: "1:683264069"
   };
   
   const fire=firebase.initializeApp(firebaseConfig)
